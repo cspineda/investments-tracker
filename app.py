e3d4a1155f3a91ff8f2ca1ba1297904cfccbcbc3
Cris Pineda <cspineda559@gmail.com>
2021-03-28 16:41:50 +0200

    added req and heroku txt

--- app.py
+++ app.py
@@ -88,6 +88,7 @@ external_stylesheets = [
 ]
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 app.title = "My Investments Profile"
+server = app.server
 
 app.layout = html.Div(
     children=[
